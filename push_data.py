81cbab41f494ea117d5799d9bb92b74b33a0ac0a
itz-gopi204 <n210204@rguktn.ac.in>
2025-01-30 16:47:06 +0530

    ETL PIPELINE CREATED

--- push_data.py
+++ push_data.py
@@ -12,11 +12,11 @@ ca=certifi.where() ##certificate authorities
 import pandas as pd
 import numpy as np
 import pymongo
-from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
+from networksecurity.exception.exception import NetworkSecurityException
 
 class NetworkDataExtract():
-    def __init_(self):
+    def __init__(self):
         try:
             pass
         except Exception as e:
@@ -28,33 +28,32 @@ class NetworkDataExtract():
             data.reset_index(drop=True,inplace=True)
             records=list(json.loads(data.T.to_json()).values())
             return records
-
-
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def insert_data_mongodb(self,records,database,collection):
+        try:
+            self.records=records
+            self.database=database
+            self.collection=collection
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            self.database=self.mongo_client[self.database]
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return(len(self.records))
         except Exception as e:
             NetworkSecurityException(e,sys)
 
 
 
-    def insert_data_mongodb(self,records,database,collection):
-        try:
-            self.database = database
-            self.collection=collection
-            self.records=records
-            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
-            self.database=self.mongo_client[self.database]
-            self.collection=self.database[self.collection]
-            self.collection.insert_many(self.records)
-            return self.records
-        except Exception as e:
-            raise NetworkSecurityException(e,sys)
 
 
 if __name__ == "__main__":
-    FILE_PATH="Network_Data\phisingData.csv"
-    DATABASE='GOPI'
-    Collection="NetworkData"
+    FILE_PATH='Network_Data\phisingData.csv'
+    DATABASE="GOPI"
+    collection='NetworkData'
     networkobj=NetworkDataExtract()
     records=networkobj.csv_to_json_converter(file_path=FILE_PATH)
     print(records)
-    no_of_records=networkobj.insert_data_mongodb(records,DATABASE,Collection)
-    print(no_of_records)
+    No_of_records=networkobj.insert_data_mongodb(records,DATABASE,collection)
+    print(No_of_records)
